6518052d970971e178c75aec12d4eed5bbdd471f
Yanama20 <bokonbaevaman@gmail.com>
2025-03-27 00:38:34 +0600

    Удаление проигнорированных файлов из репозитория

--- users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-
-
-class ConfirmationCode(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    code = models.CharField(max_length=6)
--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from users import views
-
-urlpatterns = [
-    path('registration/', views.registration_api_view),
-    path('authorization/', views.authorization_api_view),
-    path('confirm/', views.confirm_api_view),
-]
